3b5451a6c76ef394a3a1ffd260de9ea5936a1163
yihuia <156614618@qq.com>
2019-10-30 14:59:10 +0800

    测设

---
+++ src/main/java/cn/how2j/App.java
@@ -0,0 +1,13 @@
+package cn.how2j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!2121212" );
+    }
+}
